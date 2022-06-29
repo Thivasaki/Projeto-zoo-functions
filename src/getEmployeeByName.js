b1288be393d951e4b129a97ea22bd0df2f99c383
Thivasaki <thiagoivasaki@gmail.com>
2022-06-29 18:35:40 -0300

    Finalizado requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  let obj = {};
+  const find = ({ firstName, lastName }) => firstName === employeeName || lastName === employeeName;
+  const employeer = data.employees.find(find);
+  obj = { ...employeer };
+  return obj;
 }
 
 module.exports = getEmployeeByName;
