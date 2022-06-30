d6cdbc5fdd51e10269196f9bc7929c72839022ea
Thivasaki <thiagoivasaki@gmail.com>
2022-06-30 19:14:51 -0300

    Finalizado requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,19 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const obj = {};
+  data.species.forEach((element) => {
+    obj[element.name] = element.residents.length;
+  });
+  if (animal === undefined) {
+    return obj;
+  } if (animal.sex === undefined) {
+    const { specie } = animal;
+    return obj[specie];
+  }
+  const finder = data.species.find((element) => element.name === animal.specie);
+  const maleOrFemale = finder.residents.filter((resident) => resident.sex === animal.sex);
+  return maleOrFemale.length;
 }
 
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,11 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Retorna a quantidade de elefantes quando dado o parâmetro count', () => {
+    const actual = handlerElephants('count');
+
+    const expected = 4;
+
+    expect(actual).toEqual(expected);
+  });
+});
