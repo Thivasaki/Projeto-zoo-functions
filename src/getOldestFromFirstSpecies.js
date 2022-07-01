f35b781278f4f3e5bc1885e657b87a5c626d1bd0
Thivasaki <thiagoivasaki@gmail.com>
2022-07-01 18:39:55 -0300

    Finalizado requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,10 +1,12 @@
-const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
   const fider = data.employees.find((employee) => employee.id === id);
-  const animalFinder = fider
+  const responsibleForFinder = fider.responsibleFor[0];
+  const animalFinder = data.species.find((animal) => animal.id === responsibleForFinder);
+  const oldAn = animalFinder.residents.reduce((acc, curr) => ((acc.age < curr.age) ? curr : acc));
+  return Object.values(oldAn);
 }
 
 module.exports = getOldestFromFirstSpecies;
